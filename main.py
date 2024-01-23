e09a10e5003d57232ef08153a1e0230421fe4636
Nick Polliard <nick.polliard@icloud.com>
2024-01-23 15:42:14 +0000

    Adding in first route and test call to polygon

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from polygon import RESTClient
+import os
+
+
+POLYGON_KEY = os.environ.get('POLYGON_KEY', 'CANT_GET_KEY')
 
 app = Flask(__name__)
 
@@ -6,6 +11,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/aapl")
+def AAPL_data():
+    ticker = "AAPL"
+    client = RESTClient(api_key=f"{POLYGON_KEY}")
+    aggs = []
+    for a in client.list_aggs(ticker=ticker, multiplier=1,
+                              timespan="minute", from_="2023-01-01",
+                              to="2023-06-13", limit=50000):
+        aggs.append(a)
+    return aggs
 
 
 if __name__ == "__main__":
